3bd331e1f567a8bde425d2ff9deea8c0857774f2
Yuri Gabriel <yurigabrielconceicaog@gmail.com>
2026-03-11 20:04:27 -0300

    add parte do twilio

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,14 +1,16 @@
 from twilio.rest import Client
 
-account_sid = 'AC8b21e5601f64f5cccbd1589d46c25ea9'
-auth_token = '7812014608a2e41315b6856a05950ce5'
+# Criar uma conta no Twilio e preencher os campos em branco: account_sid, auth_token, content_sid e numero do whatsapp
+
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='whatsapp:+14155238886',
-  content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
+  content_sid='',
   content_variables='{"1":"12/1","2":"3pm"}',
-  to='whatsapp:+5511993081879'
+  to='whatsapp:'
 )
 
 print(message.sid)
